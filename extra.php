ffe40a57fc4b37325c54980c6777990f710b21ab
Dan PN <77532827+Derlvx@users.noreply.github.com>
2023-05-22 00:17:53 +0200

    fix extra cache

--- extra.php
+++ extra.php
@@ -205,7 +205,9 @@ fclose($fp);
     </footer>
 
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-    <script defer src="JS/data.json"></script>
+    <!-- Le php sert a empecher le stockage du fichier en cache -->
+    <script defer src="JS/data.json?random=<?= uniqid() ?>"></script>
+    <script defer src="JS/start.json"></script>
     <script defer src="JS/cart.js"></script>
     <script defer src="JS/back.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
